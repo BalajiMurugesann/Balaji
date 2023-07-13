7ed2c218aa65309bab00cd08c5d92b7f2e8c2a27
CrioUser <crio-user@criodo.com>
2023-07-13 13:32:01 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -7,7 +7,7 @@ async function init() {
   //Updates the DOM with the cities
   if (cities) {
     cities.forEach((key) => {
-      addCityToDOM(key);
+      addCityToDOM(key.id,key.city,key.description,key.image);
     });
   }
 }
@@ -16,24 +16,23 @@ async function init() {
 async function fetchCities() {
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
-  try{
-    let data = await fetch(config.backendEndpoint+"/cities")
+  try {
+    let data = await fetch(config.backendEndpoint + "/cities");
     let res = await data.json();
     return res;
-  }
-  catch(error){
+  } catch (error) {
     return null;
   }
 }
 
 //Implementation of DOM manipulation to add cities
-function addCityToDOM({id, city, description, image}) {
+function addCityToDOM( id, city, description, image ) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
-   let element=document.createElement("div");
-   element.className = "col-6 col-lg-3 mb-4";
-   element.innerHTML = `
-   <a href="pages/adventures/?city=${id}" id="${id}">
+  let element = document.createElement("div");
+  element.className = "col-6 col-lg-3 mb-4";
+  element.innerHTML = `
+   <a href="pages/adventures/?city=${id}" id=${id}>
    <div class="tile">
     <div class="title-text text-center">
     <h5>${city}</h5>
@@ -44,7 +43,7 @@ function addCityToDOM({id, city, description, image}) {
     </a>
     </div>
    `;
-   document.getElementById("data").appendChild(element);
+  document.getElementById("data").appendChild(element);
 }
 
 export { init, fetchCities, addCityToDOM };
